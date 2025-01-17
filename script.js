c88b3e208105d06b417e877e316e70b0b2988a08
Eva Schiller <eva_schiller@brown.edu>
2025-01-16 22:01:40 -0500

    small change

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('data-container');
+    container.innerHTML = '<p>Static content goes here.</p>';
+});
